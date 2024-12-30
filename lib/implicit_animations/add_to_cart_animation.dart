c256d1741deb3e62e13ef881cabc2cfdc73a2d6b
PILLAGOLLA DURGA JAYASAI <pillagolladurgajayasai@PILLAGOLLAs-MacBook-Pro.local>
2024-12-30 12:33:38 +0530

    Add to cart Animation - Implicit

---
+++ lib/implicit_animations/add_to_cart_animation.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:lottie/lottie.dart';
+
+class AddToCartAnimation extends StatefulWidget {
+  const AddToCartAnimation({super.key});
+
+  @override
+  State<AddToCartAnimation> createState() => AddToCartAnimationState();
+}
+
+class AddToCartAnimationState extends State<AddToCartAnimation> {
+  bool isClicked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(40),
+          child: AppBar(
+            title: Text(
+              "Cart Animation",
+              style: GoogleFonts.poppins(
+                  color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            centerTitle: true,
+          )),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              isClicked = !isClicked;
+            });
+          },
+          child: AnimatedContainer(
+            curve: Curves.ease,
+            duration: Duration(milliseconds: 500),
+            width: isClicked ? 200 : 80,
+            height: 60,
+            decoration: BoxDecoration(
+                color: isClicked ? Colors.green : Colors.white,
+                borderRadius: BorderRadius.circular(isClicked ? 30 : 20)),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                if (isClicked)
+                  Positioned.fill(
+                    child: Lottie.asset('assets/lottie/confetti.json',
+                        fit: BoxFit.cover),
+                  ),
+                Row(
+                  mainAxisAlignment: isClicked
+                      ? MainAxisAlignment.spaceEvenly
+                      : MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      isClicked ? Icons.check : Iconsax.bag_2,
+                      color: isClicked ? Colors.white : Colors.black,
+                    ),
+                    if (isClicked)
+                      Flexible(
+                        child: Text(
+                          "Added to cart",
+                          overflow: TextOverflow.ellipsis,
+                          style: GoogleFonts.poppins(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:animations_flutter/implicit_animations/add_to_cart_animation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Animations',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: const AppBarTheme(color: Colors.black),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+      home: const AddToCartAnimation(),
+    );
+  }
+}
